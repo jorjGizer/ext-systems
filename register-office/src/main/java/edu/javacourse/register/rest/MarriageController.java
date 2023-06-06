ffcb31a940dc202cadf5243e13263463c0872272
jorjGizer <125866680+jorjGizer@users.noreply.github.com>
2023-06-06 20:26:06 +0530

    add spring annotations ver 2

---
+++ register-office/src/main/java/edu/javacourse/register/rest/MarriageController.java
@@ -0,0 +1,20 @@
+package edu.javacourse.register.rest;
+
+import edu.javacourse.register.business.MarriageManager;
+import edu.javacourse.register.view.MarriageRequest;
+import edu.javacourse.register.view.MarriageResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("controller")
+public class MarriageController {
+    @Autowired
+    private MarriageManager marriageManager;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MarriageController.class);
+    public MarriageResponse findMarriageCertificate(MarriageRequest request){
+        LOGGER.info("findMarriageCertificate called ");
+       return marriageManager.findMarriageCertificate(request);
+    }
+}
