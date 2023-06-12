b300de3b5f8a89bf4968f75bd77225cbe2ae8fd1
jorjGizer <125866680+jorjGizer@users.noreply.github.com>
2023-06-12 12:22:10 +0530

    LazyInitialiizationException ver2

---
+++ student/src/main/java/edu/javacourse/student/servlet/UniversityListServlet.java
@@ -0,0 +1,29 @@
+package edu.javacourse.student.servlet;
+
+import edu.javacourse.student.domain.University;
+import edu.javacourse.student.service.UniversityService;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "UniversityListServlet", urlPatterns = "/universityList")
+public class UniversityListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         ServletContext ctx = getServletContext();
+        WebApplicationContext wac = WebApplicationContextUtils.getWebApplicationContext(ctx);
+        UniversityService service = wac.getBean(UniversityService.class);
+        List<University> list = service.findUniversities();
+        list.forEach(u -> System.out.println(
+                u.getUniversityId() + ":" + u.getUniversityName() + ":"+ u.getFaculties().size()));
+        getServletContext().getRequestDispatcher("/universityList.jsp").forward(req, resp);
+    }
+}
